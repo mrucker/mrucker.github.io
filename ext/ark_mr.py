4257992786590880de3bb6485e50ee8c964240a8
mrucker <rucker.mark@gmail.com>
2024-10-29 17:10:12 -0400

    Improved documentation.

--- ext/ark_mr.py
+++ ext/ark_mr.py
@@ -2,5 +2,8 @@ import ark
 import markdown2
 
 @ark.renderers.register('md')
-def render_quarto(text):
+def render_markdown(text):
+    #the two extras below are here to support syntax highlighting
+    #code-friendly: https://github.com/trentm/python-markdown2/wiki/code-friendly
+    #fenced-code-blocks: https://github.com/trentm/python-markdown2/wiki/fenced-code-blocks
     return markdown2.markdown(text,extras=["code-friendly","fenced-code-blocks"])
